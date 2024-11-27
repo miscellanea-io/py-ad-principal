557cb73c804e25cf36cd37953d64dd5f1cbe2d9b
Jason Hallford <jason.hallford@gmail.com>
2024-11-27 15:04:01 -0800

    Added dependencies to pyproject.toml and wrote expanded README.md file.

--- src/py_ad_principal_jhallford/py_ad_principal.py
+++ src/py_ad_principal_jhallford/py_ad_principal.py
@@ -1,16 +1,12 @@
 # Copyright 2024 Jason Hallford
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import logging
 import base64
 import os
@@ -47,7 +43,7 @@ class AuthenticationContextConfig:
     configuration settings from a TOML file or selectively override them via environment variables.
     """
 
-    def __init__(self, config_file: str = "ad_principal.toml"):
+    def __init__(self, config_file: str = "py_ad_principal.toml"):
         """Initializes a new Active Directory authentication context configuration.
 
         Args:
