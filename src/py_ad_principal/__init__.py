b8eb5fe33ae95bc06e2770eddb9d9b510c04a7c5
Jason Hallford <jason.hallford@gmail.com>
2024-11-28 09:55:39 -0800

    Properly exported classes and rolled version number.

--- src/py_ad_principal/__init__.py
+++ src/py_ad_principal/__init__.py
@@ -1,4 +1,4 @@
-from . import (
+from .py_ad_principal import (
     AuthenticationContextConfig,
     ActiveDirectoryError,
     ActiveDirectoryPrincipal,
@@ -6,7 +6,7 @@ from . import (
     AuthenticationContext
 )
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = [
     "AuthenticationContextConfig",
     "ActiveDirectoryError",
